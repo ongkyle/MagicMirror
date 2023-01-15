6833ad55d74208311a9c58091b0e5ae836be243e
Kyle Ong <ongkyle4@gmail.com>
2023-01-14 17:59:13 -0800

    makefiles: Add help text for targets

--- makefiles/docker.mk
+++ makefiles/docker.mk
@@ -1,4 +1,4 @@
-##@ Build
+##@ Docker
 
 # https://www.gnu.org/software/make/manual/html_node/Force-Targets.html
 # Make rules that cannot be marked phony can use this as a dependency to force them to alway run, as needed.
@@ -6,21 +6,21 @@
 .FORCE:
 
 .PHONY: docker-build
-docker-build: docker-build-artifacts docker-build-debian
+docker-build: docker-build-artifacts docker-build-debian ## Build Dockerfile-artifacts and Dockerfile-debian
 
 .PHONY: docker-build-artifacts
-docker-build-artifacts:
+docker-build-artifacts: ## Build Dockerfile-artifacts
 	./scripts/build-docker-artifacts.sh
 
 .PHONY: docker-build-debian
-docker-build-debian:
+docker-build-debian: ## Build Dockerfile-debian
 	./scripts/build-docker-debian.sh
 
 .PHONY: docker-compose
-docker-up: docker-down
+docker-up: docker-down ## Stop and start the mm container
 	cd ./docker/ && docker compose up -d
 
 .PHONY: docker-down
-docker-down:
+docker-down: ## Stop the mm container
 	cd ./docker/ && docker compose down
 
