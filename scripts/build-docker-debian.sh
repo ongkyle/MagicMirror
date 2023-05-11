de02f1b607defc915ce624493ad83f35a5dd1087
Kyle Ong <ongkyle4@gmail.com>
2023-05-10 21:40:37 -0700

    recipe: Build llama

--- scripts/build-docker-debian.sh
+++ scripts/build-docker-debian.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
 
-: ${NODE_VERSION:="18.13.0"}
 : ${DEBIAN_VERSION:="buster"}
-: ${BUILDER_IMG:="registry.gitlab.com/ongkyle/magicmirror:buildkit-artifacts-dev"}
+: ${PYTHON_VERSION:="3.10"}
+: ${BUILDER_IMG:="registry.gitlab.com/ongkyle/magicmirror:node-python${PYTHON_VERSION}-llama-${DEBIAN_VERSION}"}
 : ${IMG_TAG:="registry.gitlab.com/ongkyle/magicmirror:debian-buster-dev"}
-: ${GIT_INFO:="placeholder"}
-
+: ${NODE_ENV:=""}
 
 main() {
   docker build . \
-          --rm \
-          -f docker/Dockerfile-debian \
-          --build-arg DEBIAN_VERSION=${DEBIAN_VERSION} \
-          --build-arg BUILDER_IMG=${BUILDER_IMG} \
-          --build-arg GIT_INFO=${GIT_INFO} \
-          --build-arg NODE_VERSION=${NODE_VERSION} \
-          -t ${IMG_TAG}
+        --rm \
+        -f docker/Dockerfile-debian \
+        --build-arg BUILDER_IMG=${BUILDER_IMG} \
+        --build-arg NODE_ENV=${NODE_ENV} \
+        -t ${IMG_TAG} \
+        "$@";
 }
 
 main "$@"
